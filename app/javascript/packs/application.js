03185526bca4584f857a908c1fe76ac13d74a12d
koutika ange marie <koutikaaem@gmail.com>
2021-02-28 18:33:45 +0100

    Added the js file to be able to load the tooltips in the browser. Also I changed the loading of the bootstrap and popper files in webpacker.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,25 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
-import "bootstrap"
+//import "bootstrap"
+//import "../stylesheets/application"
+
+
+// app/javascript/packs/application.js
+import * as bootstrap from 'bootstrap'
 import "../stylesheets/application"
+
+document.addEventListener("DOMContentLoaded", function(event) {
+  var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+  var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+    return new bootstrap.Popover(popoverTriggerEl)
+  })
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+  })
+});
 
 Rails.start()
 Turbolinks.start()
